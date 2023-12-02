2c35a11e79509e5c48160bc3dc2dc198de94d98e
Jardel Bordignon <jardel1101@gmail.com>
2023-12-02 06:33:09 -0300

    criando model user e migration relacionada

---
+++ prisma/migrations/20231202093026_create_table_users/migration.sql
@@ -0,0 +1,22 @@
+-- CreateEnum
+CREATE TYPE "Role" AS ENUM ('ADMIN');
+
+-- CreateEnum
+CREATE TYPE "Permission" AS ENUM ('DELETE_USER');
+
+-- CreateTable
+CREATE TABLE "users" (
+    "id" TEXT NOT NULL,
+    "name" TEXT NOT NULL,
+    "email" TEXT NOT NULL,
+    "password" TEXT NOT NULL,
+    "roles" "Role"[],
+    "permissions" "Permission"[],
+    "created_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updated_at" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "users_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "users_email_key" ON "users"("email");
