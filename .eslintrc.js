3b1cbb14574d70b894bd214eb1509e416bfd7f65
Jardel Bordignon <jardel1101@gmail.com>
2023-12-02 05:31:30 -0300

    formatação de código

--- .eslintrc.js
+++ .eslintrc.js
@@ -5,11 +5,14 @@ module.exports = {
     tsconfigRootDir: __dirname,
     sourceType: 'module',
   },
-  plugins: ['@typescript-eslint/eslint-plugin'],
-  extends: [
-    'plugin:@typescript-eslint/recommended',
-    'plugin:prettier/recommended',
+  plugins: [
+    '@typescript-eslint/eslint-plugin',
+    'import',
+    'sort-destructure-keys',
+    'sort-keys-fix',
+    'typescript-sort-keys',
   ],
+  extends: ['plugin:@typescript-eslint/recommended', 'plugin:prettier/recommended'],
   root: true,
   env: {
     node: true,
@@ -17,9 +20,42 @@ module.exports = {
   },
   ignorePatterns: ['.eslintrc.js'],
   rules: {
+    semi: ['error', 'never'],
     '@typescript-eslint/interface-name-prefix': 'off',
     '@typescript-eslint/explicit-function-return-type': 'off',
     '@typescript-eslint/explicit-module-boundary-types': 'off',
     '@typescript-eslint/no-explicit-any': 'off',
+    'import/order': [
+      'error',
+      {
+        alphabetize: {
+          order: 'asc',
+        },
+        groups: ['builtin', 'external', 'parent', 'sibling', 'index'],
+        'newlines-between': 'always',
+        pathGroups: [
+          {
+            group: 'external',
+            pattern: 'contracts/**',
+            position: 'after',
+          },
+          {
+            group: 'external',
+            pattern: 'src/**',
+            position: 'after',
+          },
+        ],
+        pathGroupsExcludedImportTypes: ['builtin'],
+      },
+    ],
+    'sort-destructure-keys/sort-destructure-keys': [2, { caseSensitive: true }],
+    'sort-imports': [
+      'error',
+      {
+        allowSeparatedGroups: true,
+        ignoreDeclarationSort: true,
+      },
+    ],
+    'sort-keys-fix/sort-keys-fix': 'warn',
   },
-};
+}
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,16 @@
-import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
+import { Logger } from '@nestjs/common'
+import { NestFactory } from '@nestjs/core'
+
+import { AppModule } from './app.module'
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const app = await NestFactory.create(AppModule)
+  const port = 3000
+  const logger = new Logger('Bootstrap')
+
+  await app
+    .listen(port)
+    .then(async () => logger.log(`🚀 Server is running on: ${await app.getUrl()}`))
+    .catch(error => logger.error(`❌ Server starts error: ${error}`))
 }
-bootstrap();
+bootstrap()
