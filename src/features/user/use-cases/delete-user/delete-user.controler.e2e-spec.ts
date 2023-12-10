a4a4b83056d5d57bf84067000210df4654e046e9
Jardel Bordignon <jardel1101@gmail.com>
2023-12-10 11:02:08 -0300

    Ajuste teste delete-user e2e

--- src/features/user/use-cases/delete-user/delete-user.controler.e2e-spec.ts
+++ src/features/user/use-cases/delete-user/delete-user.controler.e2e-spec.ts
@@ -5,16 +5,33 @@ import supertest from 'supertest'
 import { AppModule } from 'src/app.module'
 import type { AuthUserData, CreateUserData } from 'src/contracts/account'
 
-import { USERS_URL } from '../constants'
+import { UserEntity } from '../../user.entity'
+import {
+  AUTH_URL,
+  CREATE_ADMIN_USER_DATA,
+  CREATE_REGULAR_USER_DATA,
+  USERS_URL,
+} from '../constants'
 
 describe('Delete user (E2E)', () => {
   let api: supertest.SuperTest<supertest.Test>
   let app: INestApplication
 
   let authorization: string
-  const johnEmail = 'johndoe@email.com'
-  const joeEmail = 'joesmith@email.com'
-  const password = 'Pwd@123'
+
+  const registerUser = async (data: CreateUserData) => {
+    await api.post(USERS_URL).send(data)
+  }
+
+  const authenticateUser = async (data: AuthUserData) => {
+    const response = await api.post(AUTH_URL).send(data)
+    authorization = `Bearer ${response.body.accessToken}`
+  }
+
+  const getUsers = async (): Promise<UserEntity[]> => {
+    const res = await api.get(USERS_URL).set('Authorization', authorization).send()
+    return res.body.data
+  }
 
   beforeAll(async () => {
     const moduleRef = await Test.createTestingModule({
@@ -25,39 +42,66 @@ describe('Delete user (E2E)', () => {
     api = supertest(app.getHttpServer())
 
     await app.init()
+  })
 
-    const createJohnData: CreateUserData = {
-      email: johnEmail,
-      name: 'John Doe',
-      password,
+  beforeEach(async () => {
+    await registerUser(CREATE_REGULAR_USER_DATA)
+    await registerUser(CREATE_ADMIN_USER_DATA)
+    await authenticateUser(CREATE_ADMIN_USER_DATA)
+  })
+
+  afterEach(async () => {
+    await authenticateUser(CREATE_ADMIN_USER_DATA)
+    const users = await getUsers()
+    for (const user of users) {
+      await api
+        .delete(`${USERS_URL}/${user.id}`)
+        .set('Authorization', authorization)
+        .send()
     }
-    await api.post(USERS_URL).send(createJohnData)
-
-    const createJoeData: CreateUserData = {
-      email: joeEmail,
-      name: 'Joe Smith',
-      password,
-    }
-    await api.post(USERS_URL).send(createJoeData)
-
-    const johnCredentials: AuthUserData = { email: johnEmail, password }
-    const authRes = await api.post('/auth').send(johnCredentials)
-    authorization = `Bearer ${authRes.body.accessToken}`
   })
 
   test(`[DELETE] ${USERS_URL} - success`, async () => {
-    const getUsers = await api
-      .get(USERS_URL)
-      .set('Authorization', authorization)
-      .send()
+    const users = await getUsers()
 
-    const joe = getUsers.body.data.find(user => user.email === joeEmail)
+    const regularUser = users.find(
+      user => user.email === CREATE_REGULAR_USER_DATA.email
+    )
 
     const response = await api
-      .delete(`${USERS_URL}/${joe.id}`)
+      .delete(`${USERS_URL}/${regularUser.id}`)
       .set('Authorization', authorization)
       .send()
 
     expect(response.statusCode).toBe(204)
+  })
+
+  test(`[DELETE] ${USERS_URL} - failure [non Admin try delete another account]`, async () => {
+    const newRegularUserData = {
+      email: 'james@email.com',
+      name: 'James',
+      password: 'Pwd@213',
+    }
+
+    await registerUser(newRegularUserData)
+    await authenticateUser(newRegularUserData)
+
+    const users = await getUsers()
+
+    const defaultRegularUser = users.find(
+      user => user.email === CREATE_REGULAR_USER_DATA.email
+    )
+
+    const response = await api
+      .delete(`${USERS_URL}/${defaultRegularUser.id}`)
+      .set('Authorization', authorization)
+      .send()
+
+    expect(response.statusCode).toBe(401)
+    expect(response.body).toEqual({
+      error: 'Unauthorized',
+      message: 'Only admins can delete other account.',
+      statusCode: 401,
+    })
   })
 })
