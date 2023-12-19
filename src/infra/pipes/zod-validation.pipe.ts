bda70c726438b9269ce6853798fea581272fb96e
Jardel Bordignon <jardel1101@gmail.com>
2023-12-19 19:35:41 -0300

    Ajuste na pipe dp zod

--- src/infra/pipes/zod-validation.pipe.ts
+++ src/infra/pipes/zod-validation.pipe.ts
@@ -1,23 +1,52 @@
 import { BadRequestException, PipeTransform } from '@nestjs/common'
 import { ZodError, ZodSchema, ZodType } from 'zod'
-import { fromZodError } from 'zod-validation-error'
 
 export type ZodObj<T extends Record<PropertyKey, unknown>> = {
   [key in keyof T]: ZodType<T[key]>
+}
+
+export class ZodValidationError extends BadRequestException {
+  constructor(errors: ZodError['errors']) {
+    const details = errors.map(({ path, ...rest }) => ({
+      field: path.join('.'),
+      ...rest,
+    }))
+
+    super({
+      details,
+      error: 'InputData',
+      statusCode: 400,
+    })
+  }
+
+  static example(field = 'name') {
+    const errors: ZodError['errors'] = [
+      {
+        code: 'invalid_type',
+        expected: 'string',
+        message: 'Required',
+        path: [field],
+        received: 'undefined',
+      },
+    ]
+    return new this(errors).getResponse()
+  }
 }
 
 export class ZodValidationPipe implements PipeTransform {
   constructor(private schema: ZodSchema) {}
 
   transform(value: unknown) {
+    // If it is a file, return it without validation
+    if (value && typeof value === 'object' && 'fieldname' in value) {
+      return value
+    }
+
     try {
       return this.schema.parse(value)
     } catch (error) {
       if (error instanceof ZodError) {
-        throw new BadRequestException({
-          ...fromZodError(error),
-          statusCode: 400,
-        })
+        throw new ZodValidationError(error.errors)
       }
 
       throw new BadRequestException('Validation failed')
